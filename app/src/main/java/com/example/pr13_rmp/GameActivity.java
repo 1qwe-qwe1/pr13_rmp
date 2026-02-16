0eac94e60f3641e1e99bc5f61a98dafce93ea3c0
1qwe-qwe1 <qudasetl@gmail.com>
2026-02-16 22:53:01 +0700

    The Start() method has been created

--- app/src/main/java/com/example/pr13_rmp/GameActivity.java
+++ app/src/main/java/com/example/pr13_rmp/GameActivity.java
@@ -1,6 +1,12 @@
 package com.example.pr13_rmp;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +19,39 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        try{
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e) {}
         setContentView(R.layout.activity_game);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        getWindow().setDecorFitsSystemWindows(false);
+
+        getWindow().getInsetsController().hide(
+                WindowInsets.Type.statusBars()
+        );
+    }
+
+    public boolean Started = false;
+    public boolean Finished = false;
+
+    public  void Start(View view){
+        Button button = (Button)findViewById(R.id.btnStart);
+        if(!Finished){
+            if(!Started){
+                button.setBackgroundColor(Color.RED);
+                button.setText("Пауза");
+                Started = true;
+            }
+            else if(Started){
+                button.setBackgroundColor(Color.GREEN);
+                button.setText("Старт");
+                Started = false;
+            }
+        }
+        else{
+            Intent intent=new Intent(GameActivity.this, GameActivity.class);
+            startActivity(intent);
+        }
     }
 }
